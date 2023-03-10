32185267598eb9a4073df89add186854f5dbf18d
orrvan2 <973658027@qq.com>
2023-03-10 13:09:08 +0800

    20230310

---
+++ .history/api_20230309163232.js
@@ -0,0 +1,868 @@
+import { ApolloClient, InMemoryCache, gql, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context';
+const API_URL = 'https://api.lens.dev'
+
+const authLink = setContext((_, { headers }) => {
+  const token = window.localStorage.getItem('your-storage-key')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+})
+const httpLink = createHttpLink({
+  uri: API_URL
+})
+
+
+/* create the API client */
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
+
+/*********************************************身份验证登录开********************************************/
+export const challenge = gql`
+  query Challenge($address: EthereumAddress!) {
+    challenge(request: { address: $address }) {
+      text
+    }
+  }
+`
+
+export const authenticate = gql`
+  mutation Authenticate(
+    $address: EthereumAddress!
+    $signature: Signature!
+  ) {
+    authenticate(request: {
+      address: $address,
+      signature: $signature
+    }) {
+      accessToken
+      refreshToken
+    }
+  }
+`
+/*********************************************身份验证登录关********************************************/
+
+/**************************************************获取用户信息开****************************************** */
+export const defaultProfile= gql`
+query DefaultProfile($address:EthereumAddress!) {
+  defaultProfile(request: { ethereumAddress: $address}) {
+    id
+    name
+    bio
+    isDefault
+    attributes {
+      displayType
+      traitType
+      key
+      value
+    }
+    followNftAddress
+    metadata
+    handle
+    picture {
+      ... on NftImage {
+        contractAddress
+        tokenId
+        uri
+        chainId
+        verified
+      }
+      ... on MediaSet {
+        original {
+          url
+          mimeType
+        }
+      }
+    }
+    coverPicture {
+      ... on NftImage {
+        contractAddress
+        tokenId
+        uri
+        chainId
+        verified
+      }
+      ... on MediaSet {
+        original {
+          url
+          mimeType
+        }
+      }
+    }
+    ownedBy
+    dispatcher {
+      address
+      canUseRelay
+    }
+    stats {
+      totalFollowers
+      totalFollowing
+      totalPosts
+      totalComments
+      totalMirrors
+      totalPublications
+      totalCollects
+    }
+    followModule {
+      ... on FeeFollowModuleSettings {
+        type
+        contractAddress
+        amount {
+          asset {
+            name
+            symbol
+            decimals
+            address
+          }
+          value
+        }
+        recipient
+      }
+      ... on ProfileFollowModuleSettings {
+       type
+      }
+      ... on RevertFollowModuleSettings {
+       type
+      }
+    }
+  }
+}
+`
+/**************************************************获取用户信息关****************************************** */
+/**************************************************获取用户出版物开****************************************** */
+export const publications= gql`
+query Publications($id:ProfileId,$limit:LimitScalar,$publicationTypes:[PublicationTypes!],$cursor:Cursor,$mainContentFocus:[PublicationMainFocus!]) {
+  publications(request: {
+    profileId: $id,
+    publicationTypes:$publicationTypes,
+    limit: $limit,
+    cursor:$cursor,
+    metadata: {
+      mainContentFocus: $mainContentFocus
+    }    
+  }) {
+    items {
+      __typename 
+      ... on Post {
+        ...PostFields
+      }
+      ... on Comment {
+        ...CommentFields
+      }
+      ... on Mirror {
+        ...MirrorFields
+      }
+    }
+    pageInfo {
+      prev
+      next
+      totalCount
+    }
+  }
+}
+
+fragment MediaFields on Media {
+  url
+  mimeType
+}
+
+fragment ProfileFields on Profile {
+  id
+  name
+  bio
+  attributes {
+     displayType
+     traitType
+     key
+     value
+  }
+  isFollowedByMe
+  isFollowing(who: null)
+  followNftAddress
+  metadata
+  isDefault
+  handle
+  picture {
+    ... on NftImage {
+      contractAddress
+      tokenId
+      uri
+      verified
+    }
+    ... on MediaSet {
+      original {
+        ...MediaFields
+      }
+    }
+  }
+  coverPicture {
+    ... on NftImage {
+      contractAddress
+      tokenId
+      uri
+      verified
+    }
+    ... on MediaSet {
+      original {
+        ...MediaFields
+      }
+    }
+  }
+  ownedBy
+  dispatcher {
+    address
+  }
+  stats {
+    totalFollowers
+    totalFollowing
+    totalPosts
+    totalComments
+    totalMirrors
+    totalPublications
+    totalCollects
+  }
+  followModule {
+    ...FollowModuleFields
+  }
+}
+
+fragment PublicationStatsFields on PublicationStats { 
+  totalAmountOfMirrors
+  totalAmountOfCollects
+  totalAmountOfComments
+  totalUpvotes
+  totalDownvotes
+}
+
+fragment MetadataOutputFields on MetadataOutput {
+  name
+  description
+  content
+  media {
+    original {
+      ...MediaFields
+    }
+  }
+  cover{
+    original {
+      ...CoverFields
+    }
+  }
+  attributes {
+    displayType
+    traitType
+    value
+  }
+}
+
+fragment Erc20Fields on Erc20 {
+  name
+  symbol
+  decimals
+  address
+}
+
+fragment PostFields on Post {
+  id
+  profile {
+    ...ProfileFields
+  }
+  stats {
+    ...PublicationStatsFields
+  }
+  metadata {
+    ...MetadataOutputFields
+  }
+  createdAt
+  collectModule {
+    ...CollectModuleFields
+  }
+  referenceModule {
+    ...ReferenceModuleFields
+  }
+  appId
+  hidden
+  reaction(request: null)
+  mirrors(by: null)
+  hasCollectedByMe
+}
+
+fragment MirrorBaseFields on Mirror {
+  id
+  profile {
+    ...ProfileFields
+  }
+  stats {
+    ...PublicationStatsFields
+  }
+  metadata {
+    ...MetadataOutputFields
+  }
+  createdAt
+  collectModule {
+    ...CollectModuleFields
+  }
+  referenceModule {
+    ...ReferenceModuleFields
+  }
+  appId
+  hidden
+  reaction(request: null)
+  hasCollectedByMe
+}
+
+fragment MirrorFields on Mirror {
+  ...MirrorBaseFields
+  mirrorOf {
+   ... on Post {
+      ...PostFields          
+   }
+   ... on Comment {
+      ...CommentFields          
+   }
+  }
+}
+
+fragment CommentBaseFields on Comment {
+  id
+  profile {
+    ...ProfileFields
+  }
+  stats {
+    ...PublicationStatsFields
+  }
+  metadata {
+    ...MetadataOutputFields
+  }
+  createdAt
+  collectModule {
+    ...CollectModuleFields
+  }
+  referenceModule {
+    ...ReferenceModuleFields
+  }
+  appId
+  hidden
+  reaction(request: null)
+  mirrors(by: null)
+  hasCollectedByMe
+}
+
+fragment CommentFields on Comment {
+  ...CommentBaseFields
+  mainPost {
+    ... on Post {
+      ...PostFields
+    }
+    ... on Mirror {
+      ...MirrorBaseFields
+      mirrorOf {
+        ... on Post {
+           ...PostFields          
+        }
+        ... on Comment {
+           ...CommentMirrorOfFields        
+        }
+      }
+    }
+  }
+}
+
+fragment CommentMirrorOfFields on Comment {
+  ...CommentBaseFields
+  mainPost {
+    ... on Post {
+      ...PostFields
+    }
+    ... on Mirror {
+       ...MirrorBaseFields
+    }
+  }
+}
+
+fragment FollowModuleFields on FollowModule {
+  ... on FeeFollowModuleSettings {
+    type
+    amount {
+      asset {
+        name
+        symbol
+        decimals
+        address
+      }
+      value
+    }
+    recipient
+  }
+  ... on ProfileFollowModuleSettings {
+    type
+    contractAddress
+  }
+  ... on RevertFollowModuleSettings {
+    type
+    contractAddress
+  }
+  ... on UnknownFollowModuleSettings {
+    type
+    contractAddress
+    followModuleReturnData
+  }
+}
+
+fragment CollectModuleFields on CollectModule {
+  __typename
+  ... on FreeCollectModuleSettings {
+    type
+    followerOnly
+    contractAddress
+  }
+  ... on FeeCollectModuleSettings {
+    type
+    amount {
+      asset {
+        ...Erc20Fields
+      }
+      value
+    }
+    recipient
+    referralFee
+  }
+  ... on LimitedFeeCollectModuleSettings {
+    type
+    collectLimit
+    amount {
+      asset {
+        ...Erc20Fields
+      }
+      value
+    }
+    recipient
+    referralFee
+  }
+  ... on LimitedTimedFeeCollectModuleSettings {
+    type
+    collectLimit
+    amount {
+      asset {
+        ...Erc20Fields
+      }
+      value
+    }
+    recipient
+    referralFee
+    endTimestamp
+  }
+  ... on RevertCollectModuleSettings {
+    type
+  }
+  ... on TimedFeeCollectModuleSettings {
+    type
+    amount {
+      asset {
+        ...Erc20Fields
+      }
+      value
+    }
+    recipient
+    referralFee
+    endTimestamp
+  }
+  ... on UnknownCollectModuleSettings {
+    type
+    contractAddress
+    collectModuleReturnData
+  }
+}
+
+fragment ReferenceModuleFields on ReferenceModule {
+  ... on FollowOnlyReferenceModuleSettings {
+    type
+    contractAddress
+  }
+  ... on UnknownReferenceModuleSettings {
+    type
+    contractAddress
+    referenceModuleReturnData
+  }
+  ... on DegreesOfSeparationReferenceModuleSettings {
+    type
+    contractAddress
+    commentsRestricted
+    mirrorsRestricted
+    degreesOfSeparation
+  }
+}
+`
+/**************************************************获取用户出版物关****************************************** */
+
+/***********************************获取用户收藏品 开***********************************************************/
+export const collections= gql`
+query Publications($address:EthereumAddress,$limit:LimitScalar,$publicationTypes:[PublicationTypes!],$cursor:Cursor,$mainContentFocus:[PublicationMainFocus!]) {
+  publications(request: {
+    collectedBy: $address,
+    publicationTypes:$publicationTypes,
+    limit: $limit,
+    cursor:$cursor,
+    metadata: {
+      mainContentFocus: $mainContentFocus
+    } 
+  }) {
+    items {
+      __typename 
+      ... on Post {
+        ...PostFields
+      }
+      ... on Comment {
+        ...CommentFields
+      }
+      ... on Mirror {
+        ...MirrorFields
+      }
+    }
+    pageInfo {
+      prev
+      next
+      totalCount
+    }
+  }
+}
+
+fragment MediaFields on Media {
+  url
+  mimeType
+}
+
+fragment ProfileFields on Profile {
+  id
+  name
+  bio
+  attributes {
+     displayType
+     traitType
+     key
+     value
+  }
+  isFollowedByMe
+  isFollowing(who: null)
+  followNftAddress
+  metadata
+  isDefault
+  handle
+  picture {
+    ... on NftImage {
+      contractAddress
+      tokenId
+      uri
+      verified
+    }
+    ... on MediaSet {
+      original {
+        ...MediaFields
+      }
+    }
+  }
+  coverPicture {
+    ... on NftImage {
+      contractAddress
+      tokenId
+      uri
+      verified
+    }
+    ... on MediaSet {
+      original {
+        ...MediaFields
+      }
+    }
+  }
+  ownedBy
+  dispatcher {
+    address
+  }
+  stats {
+    totalFollowers
+    totalFollowing
+    totalPosts
+    totalComments
+    totalMirrors
+    totalPublications
+    totalCollects
+  }
+  followModule {
+    ...FollowModuleFields
+  }
+}
+
+fragment PublicationStatsFields on PublicationStats { 
+  totalAmountOfMirrors
+  totalAmountOfCollects
+  totalAmountOfComments
+  totalUpvotes
+  totalDownvotes
+}
+
+fragment MetadataOutputFields on MetadataOutput {
+  name
+  description
+  content
+  media {
+    original {
+      ...MediaFields
+    }
+  }
+  attributes {
+    displayType
+    traitType
+    value
+  }
+}
+
+fragment Erc20Fields on Erc20 {
+  name
+  symbol
+  decimals
+  address
+}
+
+fragment PostFields on Post {
+  id
+  profile {
+    ...ProfileFields
+  }
+  stats {
+    ...PublicationStatsFields
+  }
+  metadata {
+    ...MetadataOutputFields
+  }
+  createdAt
+  collectModule {
+    ...CollectModuleFields
+  }
+  referenceModule {
+    ...ReferenceModuleFields
+  }
+  appId
+  hidden
+  reaction(request: null)
+  mirrors(by: null)
+  hasCollectedByMe
+}
+
+fragment MirrorBaseFields on Mirror {
+  id
+  profile {
+    ...ProfileFields
+  }
+  stats {
+    ...PublicationStatsFields
+  }
+  metadata {
+    ...MetadataOutputFields
+  }
+  createdAt
+  collectModule {
+    ...CollectModuleFields
+  }
+  referenceModule {
+    ...ReferenceModuleFields
+  }
+  appId
+  hidden
+  reaction(request: null)
+  hasCollectedByMe
+}
+
+fragment MirrorFields on Mirror {
+  ...MirrorBaseFields
+  mirrorOf {
+   ... on Post {
+      ...PostFields          
+   }
+   ... on Comment {
+      ...CommentFields          
+   }
+  }
+}
+
+fragment CommentBaseFields on Comment {
+  id
+  profile {
+    ...ProfileFields
+  }
+  stats {
+    ...PublicationStatsFields
+  }
+  metadata {
+    ...MetadataOutputFields
+  }
+  createdAt
+  collectModule {
+    ...CollectModuleFields
+  }
+  referenceModule {
+    ...ReferenceModuleFields
+  }
+  appId
+  hidden
+  reaction(request: null)
+  mirrors(by: null)
+  hasCollectedByMe
+}
+
+fragment CommentFields on Comment {
+  ...CommentBaseFields
+  mainPost {
+    ... on Post {
+      ...PostFields
+    }
+    ... on Mirror {
+      ...MirrorBaseFields
+      mirrorOf {
+        ... on Post {
+           ...PostFields          
+        }
+        ... on Comment {
+           ...CommentMirrorOfFields        
+        }
+      }
+    }
+  }
+}
+
+fragment CommentMirrorOfFields on Comment {
+  ...CommentBaseFields
+  mainPost {
+    ... on Post {
+      ...PostFields
+    }
+    ... on Mirror {
+       ...MirrorBaseFields
+    }
+  }
+}
+
+fragment FollowModuleFields on FollowModule {
+  ... on FeeFollowModuleSettings {
+    type
+    amount {
+      asset {
+        name
+        symbol
+        decimals
+        address
+      }
+      value
+    }
+    recipient
+  }
+  ... on ProfileFollowModuleSettings {
+    type
+    contractAddress
+  }
+  ... on RevertFollowModuleSettings {
+    type
+    contractAddress
+  }
+  ... on UnknownFollowModuleSettings {
+    type
+    contractAddress
+    followModuleReturnData
+  }
+}
+
+fragment CollectModuleFields on CollectModule {
+  __typename
+  ... on FreeCollectModuleSettings {
+    type
+    followerOnly
+    contractAddress
+  }
+  ... on FeeCollectModuleSettings {
+    type
+    amount {
+      asset {
+        ...Erc20Fields
+      }
+      value
+    }
+    recipient
+    referralFee
+  }
+  ... on LimitedFeeCollectModuleSettings {
+    type
+    collectLimit
+    amount {
+      asset {
+        ...Erc20Fields
+      }
+      value
+    }
+    recipient
+    referralFee
+  }
+  ... on LimitedTimedFeeCollectModuleSettings {
+    type
+    collectLimit
+    amount {
+      asset {
+        ...Erc20Fields
+      }
+      value
+    }
+    recipient
+    referralFee
+    endTimestamp
+  }
+  ... on RevertCollectModuleSettings {
+    type
+  }
+  ... on TimedFeeCollectModuleSettings {
+    type
+    amount {
+      asset {
+        ...Erc20Fields
+      }
+      value
+    }
+    recipient
+    referralFee
+    endTimestamp
+  }
+  ... on UnknownCollectModuleSettings {
+    type
+    contractAddress
+    collectModuleReturnData
+  }
+}
+
+fragment ReferenceModuleFields on ReferenceModule {
+  ... on FollowOnlyReferenceModuleSettings {
+    type
+    contractAddress
+  }
+  ... on UnknownReferenceModuleSettings {
+    type
+    contractAddress
+    referenceModuleReturnData
+  }
+  ... on DegreesOfSeparationReferenceModuleSettings {
+    type
+    contractAddress
+    commentsRestricted
+    mirrorsRestricted
+    degreesOfSeparation
+  }
+}
+`
+/***********************************获取用户收藏品 开***********************************************************/
+
+
+/********************************************************mongoose */
+
+
---
+++ .history/pages/playlist/[id]_20230310110544.js
@@ -0,0 +1,145 @@
+import styles from '../../styles/Atlas.module.css'
+import { useState, useEffect,useRef} from 'react'
+
+export default function Brochure({data,id}) {
+  console.log(data)
+  console.log(id)
+  const [loadingFlag,setLoadingFlag] = useState(false)
+  const [myProfile,setMyProfile] =useState({})
+  const [brochure,setBrochure] =useState({brochure:[],brochureTag:[],brochureName:null,category:'',likes:0,bgImg:'',content:'',slots:{},id:''})
+  const [imgLargeSiz,setImgLargeSize] =useState('')
+  const [imgLargeFlag,setImgLargeFlag] =useState(false)
+
+
+  /***第一步配置rem */
+  useEffect(() => {
+    if (data) {
+      var fz = window.innerWidth / (1440/28)
+      if(fz<16){
+        fz=16
+      }
+      if(fz>28){
+        fz=28
+      }
+      document.documentElement.style.fontSize = fz+'px'
+      fetchBrochure()
+    }
+  }, [data])
+  //这边的册子都是歌单的意思，名称我就不再更改了
+  /*****第二步将服务器返回的data装填歌单和用户 */
+  function fetchBrochure(){
+    try {
+      let tempArr = id.split('T')
+      console.log(tempArr)
+      let tempobj ={}
+      tempobj.name =data.data[0].name
+      tempobj.avatarUrl= data.data[0].avatarUrl
+      setMyProfile(tempobj)
+      let getdata = JSON.parse(data.data[0].data) ||[]
+      let brochures =getdata.songLists
+      console.log(brochures)
+      let tempbrochure = null
+      for(let i=0; i<brochures.length; i++){
+        if(brochures[i].id == id){
+          tempbrochure = brochures[i]
+          console.log(tempbrochure)
+          break
+        }
+      }
+      if(tempbrochure){
+        console.log('找到了册子')
+        setBrochure(tempbrochure) 
+        console.log(tempbrochure)
+      }else{
+        console.log('未找到该册子，可能已被删除')
+      }
+    } catch (error) {
+      console.log('error fetching brochure...', error)
+    }
+  }
+  if (!data) return null
+  return(
+    <div className={styles.cotainer}>
+      <div className={styles.header}>
+        <div className={styles.h_blank1}></div>
+        <div className={styles.h_menu}>
+          <div className={styles.div_atlas}>
+            <div className={styles.info_atlas}>PlayList</div>
+          </div>
+          <div className={styles.div_brochureName}>
+            <div className={styles.info_brochureName}>{brochure.brochureName}</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.body1}>
+        <div className={styles.body1Contanier}>
+          <div className={styles.body1Info}>
+            <div className={styles.photoFrame}>
+              {brochure.brochure[0]&&<img  src={brochure.brochure[0].src}></img>}
+            </div>
+            <div className={styles.brochureInfo}>
+              <div style={{width:'100%',height:'1.9286rem',lineHeight:'1.9286rem',color:'rgba(255,255,255,1)',fontFamily:'PingFangSC-regular',fontSize:'1.286rem'}}>{brochure.brochureName}</div>
+            <div style={{width:'100%',height:'1.786rem',}}><img style={{width:'1.7rem',height:'1.7rem',overflow:'hidden',borderRadius:'0.85rem',border:'1px solid #f1f1f1'}} src={myProfile.avatarUrl} alt={'当前网络不可用'} /></div>
+              <div style={{width:'100%',height:'3.2143rem',fontSize:'0.8571rem',textAlign:'left',fontFamily:' PingFangSC-regular',fontSize:'0.643rem',color:'rgb(255,255,255)'}}>{brochure.brochureTag[0]|| ''}<br></br>{brochure.brochureTag[1] || ''}<br></br>{brochure.brochureTag[2] || ''}</div>
+            </div>
+          </div>
+          <div className={styles.body1Contral}>
+            <div className={styles.body1Media}>
+              <div className={styles.likes}></div>
+              <div className={styles.share}></div>
+            </div>
+          </div>
+          </div>
+        </div>
+      <div className={styles.body2}>
+        <div className={styles.picturesStage}>
+          {
+            brochure &&  brochure.brochure.map((brochurePictureItem,brochurePictureIndex)=>{
+              return <div key={brochurePictureIndex}   className={styles.imgContanier}>
+                <img onClick={(e)=>{
+                  setImgLargeSize(brochurePictureItem.src)
+                  setImgLargeFlag(true)
+                }} src={brochurePictureItem.src}></img>
+                <div className={styles.imgShareBg}>
+                </div>
+                <div className={styles.imgShare}>
+                  <a target="_blank" href={'https://lenster.xyz/posts/'+brochurePictureItem.id}></a>
+                  </div>
+              </div>
+            })
+          }
+        </div>
+      </div>
+      {imgLargeFlag &&<div onClick={(e)=>{
+        setImgLargeFlag(false)
+      }} style={{position:'fixed',width:'96%',height:'96%',left:'2%',top:'2%',textAlign:'center',filter:'drop-shadow(2px 4px 6px black)'}}>
+                        <img style={{width:'100%',height:'100%',objectFit:'contain'}} src={imgLargeSiz}></img>  
+                      </div>}
+    </div>
+  ) 
+}
+export async function getServerSideProps(context){
+  const{ id } =context.query
+  // const request =require('../api/search'); 
+  // console.log(request)
+  let tempArr = id.split('T')
+  // console.log(request)
+  // console.log('gaga')
+  let user =tempArr[0]
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/search`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: user,
+    }),
+  });
+  const data = await res.json() 
+  return{
+    props:{
+      data:data,
+      id:id,
+    }
+  }
+}
